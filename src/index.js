10341512308f12566260d2a660a9f7732728367f
Daniilka48 <WellDevil48@yandex.ru>
2024-10-27 22:00:19 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const Array = [];
+  const Brackets = {};
+  
+  bracketsConfig.forEach(([open, close]) => {
+    Brackets[open] = close;
+  });
+
+  function checkChar(char) {
+    if (Brackets[char]) {
+      if (char === Brackets[char] && Array[Array.length - 1] === char) {
+        Array.pop();
+      } else {
+      Array.push(char);
+      }
+      } else {
+      const last = Array.pop();
+      if (Brackets[last] !== char) {
+        Array.push(last);
+      }
+    }
+  }
+  str.split('').forEach(checkChar);
+  return Array.length === 0;
 }
